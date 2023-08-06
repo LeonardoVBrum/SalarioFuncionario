c45e4a83bc3285f6047976b3993b7ca181b8d505
LeonardoVBrum <leonardobrum.rj@yahoo.com>
2023-08-06 13:07:44 -0300

    Adicionar arquivos de projeto.

---
+++ DadosDoFuncionarioV2/Funcioanrio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace DadosDoFuncionarioV2
+{
+    internal class Funcioanrio
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalaioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += (SalarioBruto * porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                   +", $"
+                   + SalaioLiquido().ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ DadosDoFuncionarioV2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+namespace DadosDoFuncionarioV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcioanrio pessoa = new Funcioanrio();
+
+            Console.WriteLine("Digite os dados do funcionário: !");
+            Console.Write("Nome: ");
+            pessoa.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            pessoa.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            pessoa.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Deseja aumentar o salário em quantos %: ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            pessoa.AumentarSalario(porcent);
+
+            Console.WriteLine($"Salario final= {pessoa} ");
+        }
+    }
+
+}   
